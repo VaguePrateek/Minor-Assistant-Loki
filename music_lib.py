a93414b21493897c4bda21ce95ed42c7c6dc062c
VaguePrateek <prateekmbcah@gmail.com>
2026-03-21 12:51:56 +0530

    Updated music library

--- music_lib.py
+++ music_lib.py
@@ -1,4 +1,6 @@
 music = {
-    "wolf": "https://youtu.be/2Sadfj-6wWI"
+    "wolf": "https://youtu.be/2Sadfj-6wWI",
+    "soul": "https://youtu.be/1wY4qQy9w9o",
+    "soulmate": "https://youtu.be/1wY4qQy9w9o", 
 }
    
